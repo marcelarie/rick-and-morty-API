7b932db1d7faa20732cd3173f1e699daef919c57
Marcel <googlillo@gmail.com>
2021-01-26 15:15:42 +0100

    Add origin data url to origin button

--- src/js/components/main/character.js
+++ src/js/components/main/character.js
@@ -5,6 +5,7 @@ const character = {
         </div>`,
     renderCharacter: function (data) {
         this.renderCharacterMenu()
+        console.log(data)
         const characterMenu = document.getElementById('character__menu')
         const menu = `<div id="${data.id}" class="character__menu__card">
         <div class="character__menu__card__left">
@@ -15,7 +16,13 @@ const character = {
             <p>Status: <span>${data.status}</span></p>
             <p>Species: <span>${data.species}</span></p>
             <p>Gender: <span>${data.gender}</span></p>
-            <p>Origin: <span>${data.origin.name}</span></p>
+            <p>Origin: <button data-url='${data.origin.url}'>${data.origin.name}</button></p>
+        </div>
+        <div class="character__menu__location">
+            <div class="location__name"></div>
+            <div class="location__type"></div>
+            <div class="location__dimension"></div>
+            <div class="location__residents"></div>
         </div>
         </div>`
         characterMenu.insertAdjacentHTML('beforeend', menu);
